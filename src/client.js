3632ffc0c1dac800d7981fa309f8ac78d9744d9b
Jeremy Burns <j@jaburns.net>
2019-06-19 17:27:44 -0600

    Drawing in gl

--- src/client.js
+++ src/client.js
@@ -11,6 +11,8 @@ if (__DEBUG) {
       , gl = C.getContext('webgl')
       , state
       , shaderProg
+      , vertexBuffer
+      , indexBuffer
 
     window.onresize = () => {
         C.width = window.innerWidth;
@@ -28,7 +30,7 @@ if (__DEBUG) {
 
     let compileShader = () => {
         let vertShader = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(shader[0]);
+        gl.shaderSource(vertShader, shader[0]);
         gl.compileShader(vertShader);
 
         if (__DEBUG) {
@@ -40,7 +42,7 @@ if (__DEBUG) {
         }
 
         let fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(shader[1]);
+        gl.shaderSource(fragShader, shader[1]);
         gl.compileShader(fragShader);
 
         if (__DEBUG) {
@@ -58,9 +60,39 @@ if (__DEBUG) {
         return prog;
     };
 
+    let verts = [0.2492,0,0.1102,-0.0237,0.1333,-0.081,0.04749999,-0.0506,0.0031,-0.0564,-0.0337,-0.1155,-0.003200002,-0.1747,-0.09190001,-0.149,-0.1102,-0.084,-0.158,-0.0931,-0.158,-0.0332,-0.1347,0,-0.158,0.0332,-0.158,0.0931,-0.1102,0.084,-0.09190001,0.149,-0.003200002,0.1747,-0.0337,0.1155,0.0031,0.0564,0.04749999,0.0506,0.1333,0.081,0.1102,0.0237];
+    let tris = [1,0,11,3,1,11,4,3,11,5,4,11,8,5,11,9,8,11,10,9,11,2,1,3,6,5,7,7,5,8,21,11,0,19,11,21,18,11,19,17,11,18,14,11,17,13,11,14,12,11,13,20,19,21,15,14,17,16,15,17];
+
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+    indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(tris), gl.STATIC_DRAW);
+
     let update = () => {
         gl.clearColor(0, 1, 0, 1);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        gl.useProgram(shaderProg);
+
+    //  Transform.toMatrix(ship, m4x);
+    //  Camera.getViewMatrix(camera, m4y);
+    //  mat4.mul(m4x, m4y, m4x);
+    //  Camera.getProjectionMatrix(camera, m4y);
+    //  mat4.mul(m4x, m4y, m4x);
+
+        let m4x = new Float32Array([-0.9898824095726013, -0.14189064502716064, 0, 0, 0.14189064502716064, -0.9898824095726013, 0, 0, 0, 0, -1.0002000331878662, -1, -0.7855497002601624, -1.9689877033233643, 4.9809980392456055, 5]);
+        gl.uniformMatrix4fv(gl.getUniformLocation(shaderProg, 'u_mvp'), false, m4x);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+        let posLoc = gl.getAttribLocation(shaderProd, "i_position");
+        gl.enableVertexAttribArray(posLoc);
+        gl.vertexAttribPointer(posLoc, 2, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        gl.drawElements(gl.TRIANGLES, tris.length, gl.UNSIGNED_SHORT, 0);
 
         requestAnimationFrame(update);
     };
